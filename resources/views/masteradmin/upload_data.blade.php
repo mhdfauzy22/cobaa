a4387fae54c7ed3f09a96f3b2f4370b43c7c8fc4
Rizky Sulaeman <95662101+ShiroKatsuya@users.noreply.github.com>
2025-03-14 17:43:17 +0700

    Update upload_data.blade.php
    sule helpers

--- resources/views/masteradmin/upload_data.blade.php
+++ resources/views/masteradmin/upload_data.blade.php
@@ -1,28 +1,30 @@
-<form method="POST" action="{{ route('users.store') }}">
+<form action="{{ route('users.store') }}" method="POST">
     @csrf
     <div class="modal-body">
         <div class="mb-3">
-            <label for="name" class="form-label">Name</label>
-            <input type="text" class="form-control" id="name" name="name" required>
+            <label for="name" class="form-label">Nama</label>
+            <input type="text" name="name" id="name" required>
         </div>
         <div class="mb-3">
             <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" id="email" name="email" required>
-        </div>
-        <div class="mb-3">
-            <label for="role" class="form-label">Role</label>
-            <select class="form-control" id="role" name="role" required>
-                <option value="masteradmin">masteradmin</option>
-                <option value="usertoko">usertoko</option>
-            </select>
+            <input type="email" id="email" name="email" required >
         </div>
         <div class="mb-3">
             <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" id="password" name="password">
+            <input type="password" name="password" required>
         </div>
-    </div>
-    <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary">Save changes</button>
+
+<!--         <div class="mb-3">
+            <label for="role" class="form-label">Role</label>
+            <select name="role" required>
+                <option value="" selected>Pilih Role</option>
+    		<option value="masteradmin">masteradmin</option>
+                <option value="usertoko">usertoko</option>
+            </select>
+        </div> -->
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+            <button type="submit" class="btn btn-primary">Simpan</button>
+        </div>
     </div>
 </form>
